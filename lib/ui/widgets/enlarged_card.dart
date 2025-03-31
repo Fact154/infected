bb6ff6a8df4035e55e03a00bc284e04566a8488f
hizhnyack <khizhnyack.yp@gmail.com>
2025-03-31 23:07:58 +0300

    Нормализация имён карт вынесена в отдельный класс, в директорию lib\ui\utils

--- lib/ui/widgets/enlarged_card.dart
+++ lib/ui/widgets/enlarged_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../utils/card_normalizer.dart'; // Импортируем нормализатор
 
 class EnlargedCard extends StatelessWidget {
   final String cardName;
@@ -11,31 +12,6 @@ class EnlargedCard extends StatelessWidget {
     this.imageVariant,
     Key? key,
   }) : super(key: key);
-
-  String _getImageAssetPath(String name) {
-    // Нормализуем имя файла
-    String normalizedName = name
-      .replaceAll('!', '')  // Убираем восклицательные знаки
-      .replaceAll('?', '')  // Убираем вопросительные знаки
-      .replaceAll('...', '') // Убираем многоточие
-      .replaceAll('ё', 'е') // Заменяем ё на е для файловой системы
-      .replaceAll(' ', '_') // Заменяем пробелы на подчеркивания
-      .replaceAll('.', '') // Убираем точки
-      .toLowerCase(); // Приводим к нижнему регистру
-
-    // Для карты Нечто используем специальный путь
-    if (normalizedName == 'нечто') {
-      return 'assets/cards/нечто.png';
-    }
-
-    // Для карты Заражение используем вариант изображения
-    if (normalizedName == 'заражение' && imageVariant != null) {
-      return 'assets/cards/заражение$imageVariant.png';
-    }
-
-    // Для остальных карт используем нормализованное имя
-    return 'assets/cards/$normalizedName.png';
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,7 +33,7 @@ class EnlargedCard extends StatelessWidget {
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(16),
                   child: Image.asset(
-                    _getImageAssetPath(cardName),
+                    CardNormalizer.getImageAssetPath(cardName, imageVariant: imageVariant),
                     fit: BoxFit.contain,
                     errorBuilder: (context, error, stackTrace) {
                       debugPrint('Ошибка загрузки изображения для $cardName: $error');
@@ -88,4 +64,4 @@ class EnlargedCard extends StatelessWidget {
       ),
     );
   }
-} 
+}
